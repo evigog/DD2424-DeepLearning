b9d3729a3a9b569611fb60a51818ff802834dcce
magpap <p_mel@windowslive.com>
2018-08-08 00:09:24 +0300

    numerically computed and checked grads

--- Assignment4/Assignment4.py
+++ Assignment4/Assignment4.py
@@ -38,6 +38,89 @@ def SoftMax(z):
     e = np.exp(z - np.max(z))
     return((e / e.sum(axis = 0)))
 
+class NUMgrads:
+    def __init__(self, theRNN):
+        # biases
+        self.b = np.zeros(np.shape(theRNN.b))
+        self.c = np.zeros(np.shape(theRNN.c))
+
+        # weights
+        self.U = np.zeros(np.shape(theRNN.U))
+        self.W = np.zeros(np.shape(theRNN.W))
+        self.V = np.zeros(np.shape(theRNN.V))
+
+    def ComputeGrad(self, fieldname, theRNN, seq_length, X, Y, h):
+        grad = getattr(self, fieldname) #already initialized to zeros
+        h0 = np.zeros((theRNN.m, 1))
+
+        #only if grad == 2D
+        if len(grad.shape) == 2:
+            for i in range(grad.shape[0]):
+                for j in range(grad.shape[1]):
+                    RNN_try = deepcopy(theRNN)
+                    attr_try = getattr(RNN_try, fieldname)
+                    attr_try[i, j] -= h
+                    Loss1, dummy1, dummy2, dummy3 = RNN_try.ForwardPass(X, Y, h0, seq_length)
+                    RNN_try = deepcopy(theRNN)
+                    attr_try = getattr(RNN_try, fieldname)
+                    attr_try[i, j] += h
+                    Loss2, dummy1, dummy2, dummy3 = RNN_try.ForwardPass(X, Y, h0, seq_length)
+                    grad[i, j] = (Loss2 - Loss1) / (2 * h)
+        #else if grad == 1D
+        else:
+            for i in range(grad.shape[0]):
+                RNN_try = deepcopy(theRNN)
+                attr_try = getattr(RNN_try, fieldname)
+                attr_try[i] -= h
+                Loss1, dummy1, dummy2, dummy3 = RNN_try.ForwardPass(X, Y, h0, seq_length)
+                RNN_try = deepcopy(theRNN)
+                attr_try = getattr(RNN_try, fieldname)
+                attr_try[i] += h
+                Loss2, dummy1, dummy2, dummy3 = RNN_try.ForwardPass(X, Y, h0, seq_length)
+                grad[i] = (Loss2 - Loss1) / (2 * h)
+
+    #given code in matlab
+    # function grad = ComputeGradNum(X, Y, f, RNN, h) #f einai to sygkekrimeno fieldname #h einai
+    #     n = numel(RNN.(f));
+    #     grad = zeros(size(RNN.(f)));
+    #     hprev = zeros(size(RNN.W, 1), 1);
+    #     for i=1:nRNN_try = RNN;
+    #         RNN_try.(f)(i) = RNN.(f)(i) - h;
+    #         l1 = ComputeLoss(X, Y, RNN_try, hprev);
+    #         RNN_try.(f)(i) = RNN.(f)(i) + h;
+    #         l2 = ComputeLoss(X, Y, RNN_try, hprev);
+    #         grad(i) = (l2 - l1) / (2 * h)
+
+
+    def Compute(self, theRNN, seq_length, X, Y, h):
+        fieldnames = list(self.__dict__.keys())
+        for i in range(len(fieldnames)):
+            self.ComputeGrad(fieldnames[i], theRNN, seq_length, X, Y, h)
+
+    def Compare(self, theGrads):
+        fieldnames = list(self.__dict__.keys())
+        for f in range(len(fieldnames)):
+            num_grad = getattr(self, fieldnames[f])
+            grad = getattr(theGrads, fieldnames[f])
+
+            print(fieldnames[f])
+            # only if grad == 2D
+            if len(grad.shape) == 2:
+                for i in range(np.shape(grad)[0]):
+                    for j in range(np.shape(grad)[1]):
+                        diff = abs(grad[i, j] - num_grad[i, j]) / max(1e-6, (abs(grad[i, j]) + abs(num_grad[i, j])))
+                        if (diff > 1e-6):
+                            print(i, j, grad[i, j], num_grad[i, j], diff)
+            #else if grad == 1D
+            else:
+                for i in range(np.size(grad)):
+                    diff = abs(grad[i] - num_grad[i]) / max(1e-6, (abs(grad[i]) + abs(num_grad[i])))
+                    if (diff > 1e-6):
+                        print(i, grad[i], num_grad[i], diff)
+
+
+
+
 class RNNgrads:
     def __init__(self, theRNN, seq_length):
         # biases
@@ -75,22 +158,59 @@ class RNNgrads:
 
 
 class RNN:
-    def __init__(self, letter_length, hidden_length):
+    def __init__(self, letter_length, hidden_length, **kwargs):
+        #first, default constructor
+        if len(kwargs) == 0:
 
-        # dimensionality of RNN's hidden state
-        self.m = hidden_length
+            # dimensionality of RNN's hidden state
+            self.m = hidden_length
 
-        # input and output digit length - equal to one-hot representation of a letter
-        self.K = letter_length
+            # input and output digit length - equal to one-hot representation of a letter
+            self.K = letter_length
 
-        # biases
-        self.b = np.zeros((self.m, 1))
-        self.c = np.zeros((self.K, 1))
+            # biases
+            self.b = np.zeros((self.m, 1))
+            self.c = np.zeros((self.K, 1))
 
-        # weights
-        self.U = np.random.normal(0, 0.01, (self.m, self.K))
-        self.W = np.random.normal(0, 0.01, (self.m, self.m))
-        self.V = np.random.normal(0, 0.01, (self.K, self.m))
+            # weights
+            self.U = np.random.normal(0, 0.01, (self.m, self.K))
+            self.W = np.random.normal(0, 0.01, (self.m, self.m))
+            self.V = np.random.normal(0, 0.01, (self.K, self.m))
+
+        #constructor used for deep copying
+        else:
+            # dimensionality of RNN's hidden state
+            self.m = hidden_length
+
+            # input and output digit length - equal to one-hot representation of a letter
+            self.K = letter_length
+
+            # biases
+            self.b = kwargs['b']
+            self.c = kwargs['c']
+
+            # weights
+            self.U = kwargs['U']
+            self.W = kwargs['W']
+            self.V = kwargs['V']
+
+
+    # this is copied (but modified) from stackoverflow
+    def __deepcopy__(self, memo):  # memo is a dict of id's to copies
+        id_self = id(self)  # memoization avoids unnecessary recursion
+        _copy = memo.get(id_self)
+        if _copy is None:
+            _copy = type(self)(
+                deepcopy(self.K, memo),
+                deepcopy(self.m, memo),
+                b=deepcopy(self.b, memo),
+                c=deepcopy(self.c, memo),
+                U=deepcopy(self.U, memo),
+                W=deepcopy(self.W, memo),
+                V=deepcopy(self.V, memo))
+            memo[id_self] = _copy
+        return _copy
+
 
     def BasicForwPass(self, hprev, xt):
         at = np.dot(self.W, hprev) + np.dot(self.U, xt) + self.b
@@ -152,7 +272,7 @@ def Main():
     #RNN sequence length
     seq_length = 25# given
     #dimensionality of hidden state
-    hidden_length = 100# given
+    hidden_length = 5#to check grads #100# given for default training
 
     # learning rate
     eta = 0.1  # default
@@ -181,10 +301,15 @@ def Main():
         Y[:, i] = CharToOneHot(Y_chars[i], char_to_ind, letter_length).flatten()
 
     Loss, a, h, p = theRNN.ForwardPass(X, Y, h0, seq_length)
-    print(Loss)
 
     theGrads = RNNgrads(theRNN, seq_length)
     theGrads.Compute(theRNN, seq_length, X, Y, a, h, p)
+
+    theNumGrads = NUMgrads(theRNN)
+    theNumGrads.Compute(theRNN, seq_length, X, Y, h=1e-4)
+    theNumGrads.Compare(theGrads)
+
+
 
 Main()
 
